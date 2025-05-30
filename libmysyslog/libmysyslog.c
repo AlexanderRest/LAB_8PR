d16c0554659c985e18d20469c84725e940fc35a6
AlexanderRest <restzone0964@gmail.com>
2025-05-30 14:10:37 +0300

    3rd commit

--- libmysyslog/libmysyslog.c
+++ libmysyslog/libmysyslog.c
@@ -3,35 +3,39 @@
 #include <string.h>
 #include <time.h>
 #include "libmysyslog.h"
-int mysyslog(const char* msg, int level, int driver, int format, const char* path) {
 
- // Реализация логов
- FILE *log_file;
- log_file = fopen(path, "a");
- if (log_file == NULL) {
- return -1;
- }
- time_t now;
- time(&now);
- char *timestamp = ctime(&now);
- timestamp[strlen(timestamp) - 1] = '\0'; // Удаление символа новой строки
- const char *level_str;
- switch (level) {
- case DEBUG: level_str = "DEBUG"; break;
- case INFO: level_str = "INFO"; break;
- case WARN: level_str = "WARN"; break;
- case ERROR: level_str = "ERROR"; break;
- case CRITICAL: level_str = "CRITICAL"; break;
- default: level_str = "UNKNOWN"; break;
- }
- if (format == 0) {
- // Текстовый формат
- fprintf(log_file, "%s %s %d %s\n", timestamp, level_str, driver, msg);
- } else {
- // Формат JSON
- fprintf(log_file, "{\"timestamp\":\"%s\",\"log_level\":\"%s\",\"driver\":%d,\"message\":\"%s\"}\n",
-timestamp, level_str, driver, msg);
- }
- fclose(log_file);
- return 0;
+int mysyslog(const char* msg, int level, int driver, const char* path) {
+    if (msg == NULL || path == NULL) {
+        return -1;
+    }
+
+    FILE *log_file = fopen(path, "a");
+    if (log_file == NULL) {
+        return -1;
+    }
+
+    time_t now;
+    time(&now);
+    char *timestamp = ctime(&now);
+    if (timestamp != NULL) {
+        timestamp[strlen(timestamp) - 1] = '\0';
+
+    const char *level_str;
+    switch (level) {
+        case DEBUG:    level_str = "DEBUG"; break;
+        case INFO:     level_str = "INFO"; break;
+        case WARN:     level_str = "WARN"; break;
+        case ERROR:    level_str = "ERROR"; break;
+        case CRITICAL: level_str = "CRITICAL"; break;
+        default:       level_str = "UNKNOWN"; break;
+    }
+
+    if (timestamp != NULL) {
+        fprintf(log_file, "%s %s %d %s\n", timestamp, level_str, driver, msg);
+    } else {
+        fprintf(log_file, "NULL %s %d %s\n", level_str, driver, msg);
+    }
+
+    fclose(log_file);
+    return 0;
 }
