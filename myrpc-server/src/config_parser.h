303f0ee5c0bc3730010c989b12981225afa4a22b
AlexanderRest <restzone0964@gmail.com>
2025-06-06 13:18:44 +0300

    2nd commit

--- myrpc-server/src/config_parser.h
+++ myrpc-server/src/config_parser.h
@@ -5,10 +5,9 @@
 
 typedef struct {
     int port;
-    char socket_type[MAX_STRING_LEN];
-    char user[MAX_STRING_LEN]; // Добавлено поле для пользователя
+    const char *socket_type;
 } Config;
 
 Config parse_config(const char *filename);
 
-#endif // CONFIG_PARSER_H
+#endif
